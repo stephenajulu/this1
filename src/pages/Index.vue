883f510f381030b01b0d2534ae15218d576c7a72
Stephen Ajulu <alunje73@gmail.com>
2020-07-03 14:14:04 +0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,14 +32,14 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: 'The Tropical Health Information System'
     }
   },
   metaInfo() {
     return {
       title: this.description,
       meta: [
-        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+        { key: 'description', name: 'description', content: 'THIS is a Tropical Health Information System that aims to solve misdiagnosis and a lack of information through our database that holds information related to tropical diseases, signs, symptoms, their possible solutions(both pharmacetical or herbal), available vaccines, and more. ' }
       ]
     }
   }
